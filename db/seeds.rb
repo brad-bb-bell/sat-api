e64189f91d4a444904f0500075396712fb694220
brad-bb-bell <brad.bell5@montana.edu>
2023-02-22 15:42:01 -0700

    changed db to production and added a password for user 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-User.create!(name: "BB", email: "brad.bb.bell@gmail.com")
+User.create!(name: "BB", email: "brad.bb.bell@gmail.com", password: "password")
 
 Activity.create!(name: "yoga", user_id: 1)
 Activity.create!(name: "hike the M", user_id: 1)
